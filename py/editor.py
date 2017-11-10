17bbeedabc8982992f10d35f52f7f06bc76a95a6
Rohan Drape <rd@slavepianos.org>
2017-11-10 16:01:39 +1100

    ~py (+clarity)

--- py/editor.py
+++ py/editor.py
@@ -7,16 +7,38 @@ import os.path
 
 cgitb.enable()
 
-print "Content-type: text/html; charset=utf-8"
-print
+print("Content-type: text/html; charset=utf-8")
+print("")
 
-print """
+h_begin = """
 <html>
 <head>
 <title>text editor</title>
 </head>
 <body>
 """
+
+def h_editor (fn,txt) :
+    ("""
+    <p>q_fn=%(fn)s</p>
+    <form accept-charset="utf-8" method="post" enctype="multipart/form-data" action="index.cgi">
+    <textarea name="f_text" rows="30" cols="80">%(txt)s</textarea>
+    <br />
+    <input type="hidden" name="f_filename" value="%(fn)s" />
+    <input type="submit" name="f_edit" value="f_edit" />
+    </form>
+    """ % {'fn': fn, 'txt': txt})
+
+def h_result (fn,txt) :
+    ("""
+    <p>fn=%(fn)s; file stored</p>
+    <pre>%(txt)s</pre>
+    """ % {'fn': fn, 'txt': txt})
+
+h_end = """
+    </body>
+    </html>
+    """
 
 # q = query, f = form
 
@@ -27,18 +49,11 @@ if q_fn != "" and os.path.isfile(q_fn) :
 
     q_fp = io.open(q_fn, mode="r", encoding="utf-8")
     file_txt = q_fp.read()
+    q_fp.close()
 
-    print("""
-  <p>q_fn=%(q_fn)s</p>
-  <form accept-charset="utf-8" method="post" enctype="multipart/form-data" action="index.cgi">
-  <textarea name="f_text" rows="30" cols="80">%(file_txt)s</textarea>
-  <br />
-  <input type="hidden" name="f_filename" value="%(q_fn)s" />
-  <input type="submit" name="f_edit" value="f_edit" />
-  </form>
-</body>
-</html>
-    """ % {'q_fn': q_fn, 'file_txt': file_txt})
+    print(h_begin)
+    print(h_editor(q_fn, file_txt))
+    print(h_end)
 
 else:
 
@@ -48,9 +63,7 @@ else:
     f_fp.write(result_txt)
     f_fp.close()
 
-    print("""
-  <p>f_fn=%(f_fn)s; file stored</p>
-  <pre>%(result_txt)s</pre>
-</body>
-</html>
-    """ % {'f_fn': f_fn, 'result_txt': result_txt})
+    print(h_begin)
+    print(h_result(f_fn,result_txt))
+    print(h_end)
+
